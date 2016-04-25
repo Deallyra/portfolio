f9e0f23454c4325fc21f0956edf05eb67a8fdb25
Deallyra <wg_aphrodite@hotmail.com>
2016-04-25 17:58:52 +0200

    Corrections XS

--- formations.php
+++ formations.php
@@ -3,10 +3,10 @@
 
     <article class="formation">
         <div class="row">
-            <div class="col-xs-2 col-sm-1">
+            <div class="col-xs-12 col-sm-1">
                 <div class="date">2010</div>
             </div>
-            <div class="col-xs-10 col-sm-11">
+            <div class="col-xs-12 col-sm-11">
                 <h3>Licence professionnelle Activités et Techniques de Communications <small>Option web design</small></h3>
                 <div class="lieu">Serfa et IUT, Mulhouse</div>
             </div>
@@ -15,10 +15,10 @@
 
     <article class="formation">
         <div class="row">
-            <div class="col-xs-2 col-sm-1">
+            <div class="col-xs-12 col-sm-1">
                 <div class="date">2008</div>
             </div>
-            <div class="col-xs-10 col-sm-11">
+            <div class="col-xs-12 col-sm-11">
                 <h3>BTS Informatique de Gestion <small>Option développement d'applications</small></h3>
                 <div class="lieu">Lycée Camille Sée, Colmar</div>
             </div>
@@ -27,10 +27,10 @@
 
     <article class="formation">
         <div class="row">
-            <div class="col-xs-2 col-sm-1">
+            <div class="col-xs-12 col-sm-1">
                 <div class="date">2006</div>
             </div>
-            <div class="col-xs-10 col-sm-11">
+            <div class="col-xs-12 col-sm-11">
                 <h3>BAC Sciences et Technologies Tertiaires <small>Option comptabilité-gestion</small></h3>
                     <div class="lieu">Lycée Marguerite Yourcenar, Erstein</div>
             </div>
