c01690b515285a1708e0ea5d86ed3b76791be74f
Deallyra <wg_aphrodite@hotmail.com>
2016-04-26 16:49:03 +0200

    Modifications tablettes

--- langues.php
+++ langues.php
@@ -2,22 +2,22 @@
     <h2>Langues étrangères</h2>
     <article class="langue">
         <div class="row">
-            <div class="col-xs-7 col-sm-12 col-lg-5">
-                <?php notes($note = 4); ?>
-            </div>
             <div class="col-xs-5 col-sm-12 col-lg-7">
                 <h3>Anglais écrit</h3>
+            </div>
+            <div class="col-xs-7 col-sm-12 col-lg-5">
+                <?php notes($note = 4); ?>
             </div>
         </div>
     </article>
 
     <article class="logiciel">
         <div class="row">
-            <div class="col-xs-7 col-sm-12 col-lg-5">
-                <?php notes($note = 3); ?>
-            </div>
             <div class="col-xs-5 col-sm-12 col-lg-7">
                 <h3>Anglais oral</h3>
+            </div>
+            <div class="col-xs-7 col-sm-12 col-lg-5">
+                <?php notes($note = 3); ?>
             </div>
         </div>
     </article>
--- logiciels.php
+++ logiciels.php
@@ -2,33 +2,33 @@
     <h2>Logiciels</h2>
     <article class="logiciel">
         <div class="row">
-            <div class="col-xs-7 col-sm-12 col-lg-5">
-                <?php notes($note = 4); ?>
-            </div>
             <div class="col-xs-5 col-sm-12 col-lg-7">
                 <h3>MySQL Workbench</h3>
             </div>
-        </div>
-    </article>
-
-    <article class="logiciel">
-        <div class="row">
             <div class="col-xs-7 col-sm-12 col-lg-5">
                 <?php notes($note = 4); ?>
             </div>
+        </div>
+    </article>
+
+    <article class="logiciel">
+        <div class="row">
             <div class="col-xs-5 col-sm-12 col-lg-7">
                 <h3>NetBeans</h3>
+            </div>
+            <div class="col-xs-7 col-sm-12 col-lg-5">
+                <?php notes($note = 4); ?>
             </div>
         </div>
     </article>
 
     <article class="logiciel">
         <div class="row">
-            <div class="col-xs-7 col-sm-12 col-lg-5">
-                <?php notes($note = 3); ?>
-            </div>
             <div class="col-xs-5 col-sm-12 col-lg-7">
                 <h3>Photoshop</h3>
+            </div>
+            <div class="col-xs-7 col-sm-12 col-lg-5">
+                <?php notes($note = 3); ?>
             </div>
         </div>
     </article>
